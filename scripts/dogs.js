328f8af6d4ff268cc560dbe2722f4e25e38bd6d0
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2023-12-04 14:31:38 -0330

    some bugs fixed

--- scripts/dogs.js
+++ scripts/dogs.js
@@ -35,3 +35,36 @@ function loadImages(breed, numImages) {
     document.querySelector("#images").innerHTML = output;
   });
 }
+
+
+// function loadImages(breed, numImages) {
+//   const imagesDiv = document.querySelector("#images");
+//   if (numImages < 1 || numImages > 100)  {
+//     errorMessage.innerHTML = "Please enter a number between 1-100";
+//     setTimeout(() => {
+//       errorMessage.innerHTML = "";
+//     }, 5000);
+//     return;
+//   }
+
+//   while (imagesDiv.firstChild) {
+//     imagesDiv.removeChild(imagesDiv.firstChild);
+//   }
+  
+//   fetch(`https://dog.ceo/api/breed/${breed}/images/random/${numImages} `) 
+//   .then((response) => {
+//    return response.json();
+//   })
+//   .then((data) => {
+//     data.message.forEach(imageURL => {
+//       const img = document.createElement('img');
+//       img.src = imageURL;
+//       // document.querySelector("#images");//.appendChild(img);
+//     });
+//   });
+// }
+
+// document.querySelector("#dog-breeds").addEventListener("change", function() {
+//   const numImages = document.querySelector("#num-dogs").value;
+//   loadImages(this.value, numImages);
+// });
--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,50 @@
-
 document.querySelector(".btn").addEventListener("click", loadAPI);
 
 function loadAPI() {
   fetch("https://dog.ceo/api/breeds/list/all")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let output = "";
-    const breeds = Object.keys(data.message);
-    breeds.forEach((breed) => {
-      output += `<option value="${breed}">${breed}</option>`;
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let output = "";
+      const breeds = Object.keys(data.message);
+      breeds.forEach((breed) => {
+        output += `<option value="${breed}">${breed}</option>`;
+      });
+      document.querySelector("#dog-breeds").innerHTML = output;
     });
-    document.querySelector("#dog-breeds").innerHTML = output;
-  });
 }
 
 function loadImages(breed, numImages) {
-  const imagesDiv = document.querySelector("#images");
-  if (numImages < 1 || numImages > 100)  {
-    alert("Please enter a number between 1-100");
-    throw new Error('Please enter a number between 1-100');
+
+  if (numImages < 1 || numImages > 100) {
+    errorMessage.innerHTML = "Please enter a number between 1-100";
+    setTimeout(() => {
+      errorMessage.innerHTML = "";
+    }, 5000);
+
+    return;
   }
-  
+
+  const imagesDiv = document.querySelector("#images");
   while (imagesDiv.firstChild) {
     imagesDiv.removeChild(imagesDiv.firstChild);
   }
-  
-  fetch(`https://dog.ceo/api/breed/${breed}/images/random/${numImages -1}`) 
-  .then((response) => {
-   return response.json();
-  })
-  .then((data) => {
-    data.message.forEach(imageURL => {
-      const img = document.createElement('img');
-      img.src = imageURL;
-      document.querySelector("#images").appendChild(img);
+
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random/${numImages}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let output = "";
+      data.message.forEach((imageURL) => {
+        output += `<img src="${imageURL}">`;
+        document.querySelector("#images").innerHTML = output;
+      });
     });
-  });
 }
 
-var numDogs = "";
-document.getElementById("num-dogs").value = numDogs;
-
-document.querySelector("#dog-breeds").addEventListener("change", function() {
+document.querySelector("#dog-breeds").addEventListener("change", function () {
   const numImages = document.querySelector("#num-dogs").value;
   loadImages(this.value, numImages);
 });
